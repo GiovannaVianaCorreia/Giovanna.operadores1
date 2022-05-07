dd10adfe434f0483dcc4eef8f7f0defda08768e9
Giovanna <102827629+GiovannaVianaCorreia@users.noreply.github.com>
2022-05-07 12:26:53 -0300

    Add files via upload

---
+++ operadores.py
@@ -0,0 +1,13 @@
+#Faça um que ajude uma empresa de limpeza. Para isso o deve ler um comprimento e comprimento de um cômodo e calcular e mostrar uma área ser limpa e a quantidade de produto necessária para o serviço, sabendo que cada litro de produto uma área de 2 metros quadrados.
+
+ainput = input ( "Digite a largura do cômodo em metros: " )
+binput = input ( "Digite o comprimento do cômodo em metros: " )
+
+a = float ( aentrada )
+b = float ( binput )
+
+área = a * b
+
+produto = área / 2
+
+print ( "Para limpar essa área você pode entregar (produto) + " litros do produto )
